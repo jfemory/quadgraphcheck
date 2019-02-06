b89765c79ffa620554271742b0276aba066ae315
John Emory <jfemory@gmail.com>
2019-02-06 03:31:02 -0600

    Something workable, with stats

--- simpleSort.go
+++ simpleSort.go
@@ -2,31 +2,122 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+	"encoding/csv"
+	//	"fmt"
 	"log"
 	"os"
 	"strconv"
-	"sync"
 	"strings"
+	"sync"
 )
 
 //funcGraph is a struct that holds the reduced graph information for each constant
 //for a given prime. The prime is not included as part of the constPackage, as there
 //is an array of these associated with a given prime.
 type preP struct {
-	constant        int
-	critCycleLength int
+	constant        []int
 	critHeight      int
+	critCycleLength int
 	//components      []block
 	//tempList	[][]int //edgeset is temporary and decrements as components are built.
 }
 
-func main() {
-	p := 17
-	fmt.Print(buildPrimePortrait(p))
+type outputData struct {
+	p                   int
+	h_avg               float64
+	h_max               int
+	n_avg               float64
+	n_max               int
+	t_avg               float64
+	t_max               int
+	singletonRatio      float64
+	nonsingletonClasses int
 }
 
-//sortIt takes a prime p, builds an array of preperiodic portraits from it,
+func main() {
+	file, err := os.Create("preperiodicPortraitStats.csv")
+	checkError("Cannot Create File", err)
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	primeChan := make(chan int)
+	//listEmpty := make(chan bool)
+	out := make(chan []string)
+
+	go parsePrimeList(primeChan)
+	//go writeIt(out, writer)
+	//go writeIt(out, writer)
+
+	writer.Write([]string{"p", "h_avg", "h_max", "n_avg", "n_max", "t_avg", "t_max", "singleton_ratio", "nonsingletonClasses"})
+
+	//p := 11
+	for {
+		p := <-primeChan
+		port := buildPrimePortrait(p)
+		go scorePrimePortrait(p, port, out)
+		writer.Write(<-out)
+	}
+}
+
+//sortPrimePortrait takes a prime p, calls buildPrimePortrait to construct an array of
+//func sortPrimePortrait(p prime)
+
+//scorePrimePortrait takes a prime portrait slice, port, and puts outputData onto the outData channel
+func scorePrimePortrait(p int, port []preP, outdata chan<- []string) {
+	var out outputData
+	h_max := 0
+	h_sum := 0
+	n_max := 1
+	n_sum := 1
+	t_sum := 0
+	t_count := 0
+	t_max := 0
+	singleton_count := 1
+	out.p = p
+
+	for i := 0; i < len(port); i++ {
+		//set coefficient for sums
+		coeff := len(port[i].constant)
+		//increment x_sum and x_count
+		h_sum = h_sum + (coeff * port[i].critHeight)
+		n_sum = n_sum + (coeff * port[i].critCycleLength)
+		if len(port[i].constant) != 1 {
+			t_sum = t_sum + len(port[i].constant)
+			t_count++
+		} else {
+			singleton_count++
+		}
+		//update x_max
+		if h_max < port[i].critHeight {
+			h_max = port[i].critHeight
+		}
+		if n_max < port[i].critCycleLength {
+			n_max = port[i].critCycleLength
+		}
+		if t_max < len(port[i].constant) {
+			t_max = len(port[i].constant)
+		}
+	}
+	out.h_avg = float64(h_sum) / float64(p)
+	out.h_max = h_max
+	out.n_avg = float64(n_sum) / float64(p)
+	out.n_max = n_max
+	if t_count == 0 {
+		out.t_avg = 1
+	} else {
+		out.t_avg = float64(t_sum) / float64(t_count)
+	}
+	out.t_max = t_max
+	out.singletonRatio = float64(singleton_count) / float64(p)
+	out.nonsingletonClasses = t_count
+	outdata <- []string{strconv.Itoa(out.p), strconv.FormatFloat(out.h_avg, 'f', -1, 64), strconv.Itoa(out.h_max), strconv.FormatFloat(out.n_avg, 'f', -1, 64), strconv.Itoa(out.n_max), strconv.FormatFloat(out.t_avg, 'f', -1, 64), strconv.Itoa(out.t_max), strconv.FormatFloat(out.singletonRatio, 'f', -1, 64), strconv.Itoa(out.nonsingletonClasses)}
+}
+
+//outputParser converts raw type into a string array
+//func outputParser (outdata <- []string{strconv.Itoa(out.p), strconv.FormatFloat(out.h_avg, 'f', -1, 64), strconv.Itoa(out.h_max), strconv.FormatFloat(out.n_avg, 'f', -1, 64), strconv.Itoa(out.n_max), strconv.FormatFloat(out.t_avg, 'f', -1, 64), strconv.Itoa(out.t_max), strconv.FormatFloat(out.singletonRatio, 'f', -1, 64), strconf.Itoa(out.nonsingletonClasses)}
+//}
+
 //buildPrimePortrait builds an array of preperiodic portraits from channel of preperiodic portraits, as they come in. It returns this array.
 func buildPrimePortrait(p int) []preP {
 	primePortrait := make([]preP, 0)
@@ -36,7 +127,18 @@ func buildPrimePortrait(p int) []preP {
 		go preperiod(p, i, portrait, &wg)
 	}
 	for i := 1; i < p; i++ {
-		primePortrait = append(primePortrait, <-portrait)
+		flag := false
+		new := <-portrait
+		for j := 0; j < len(primePortrait); j++ {
+			if primePortrait[j].critCycleLength == new.critCycleLength && primePortrait[j].critHeight == new.critHeight {
+				flag = true
+				primePortrait[j].constant = append(primePortrait[j].constant, new.constant[0])
+			}
+
+		}
+		if flag == false {
+			primePortrait = append(primePortrait, new)
+		}
 	}
 	wg.Wait()
 	close(portrait)
@@ -54,7 +156,7 @@ func preperiod(p int, c int, portrait chan<- preP, wg *sync.WaitGroup) {
 		new = (cycleCheck[i]*cycleCheck[i] + c) % p
 		for j := 0; j < len(cycleCheck); j++ {
 			if new == cycleCheck[j] {
-				portrait <- preP{c, (len(cycleCheck) - j), j}
+				portrait <- preP{[]int{c}, (len(cycleCheck) - j), j}
 				wg.Done()
 				return
 			}
@@ -65,6 +167,7 @@ func preperiod(p int, c int, portrait chan<- preP, wg *sync.WaitGroup) {
 
 //parsePrimeList takes a list of primes and pushes them one by one onto primeChan
 func parsePrimeList(primeChan chan int) {
+	defer close(primeChan)
 	//open file logic
 	openFile, err := os.Open("list.prime")
 	checkError("Failed to open prime list file.", err)
@@ -74,18 +177,23 @@ func parsePrimeList(primeChan chan int) {
 	for scanner.Scan() {
 		stringSlice := (strings.Split(strings.Trim(scanner.Text(), "[]"), " "))
 		for i := 0; i < len(stringSlice); i++ {
-			prime, err := strconv.Atoi(stringSlice[i])
-			checkError("Problem converting file into primes.", err)
-			fmt.Println(prime)
+			prime, _ := strconv.Atoi(stringSlice[i])
 			primeChan <- prime
 		}
 	}
-	close(primeChan)
 	//	checkError("bufio problem, figure it out...", err)
 }
 
 func checkError(message string, err error) {
 	if err != nil {
 		log.Fatal(message, err)
+	}
+}
+
+func writeIt(data chan []string, writer *csv.Writer) {
+	for {
+		output := <-data
+		err := writer.Write(output)
+		checkError("Write to file failed.", err)
 	}
 }
